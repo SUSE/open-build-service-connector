0d43455118715360dfde8a4dfa90be821aba9a9b
Dan Čermák <dcermak@suse.com>
2020-01-17 16:49:57 +0100

    Add Base clases offering logging & listening to connection changes

--- src/base-components.ts
+++ src/base-components.ts
@@ -1,6 +1,42 @@
 import { Logger } from "pino";
+import * as vscode from "vscode";
+import { ApiAccountMapping } from "./accounts";
 
 /** Base class for components that should have access to the logger */
 export class LoggingBase {
   constructor(protected readonly logger: Logger) {}
+}
+
+export class DisposableBase {
+  protected disposables: vscode.Disposable[] = [];
+
+  public dispose() {
+    this.disposables.forEach(disp => disp.dispose());
+  }
+}
+
+export class ConnectionListenerBase extends DisposableBase {
+  protected currentConnections: ApiAccountMapping = {
+    defaultApi: undefined,
+    mapping: new Map()
+  };
+
+  constructor(onConnectionChange: vscode.Event<ApiAccountMapping>) {
+    super();
+
+    const disposable = onConnectionChange(apiAccountMapping => {
+      this.currentConnections = apiAccountMapping;
+    }, this);
+
+    this.disposables.push(disposable);
+  }
+}
+
+export class ConnectionListenerLoggerBase extends ConnectionListenerBase {
+  constructor(
+    onConnectionChange: vscode.Event<ApiAccountMapping>,
+    protected readonly logger: Logger
+  ) {
+    super(onConnectionChange);
+  }
 }
