ac3e10cf44b50b8e44ca8afd952e457bf2c11593
Dan Čermák <dcermak@suse.com>
2020-02-06 14:36:43 +0100

    Add fakeEvent & create stubbed vscodeWindow helpers for tests

--- src/test/suite/test-utils.ts
+++ src/test/suite/test-utils.ts
@@ -2,6 +2,7 @@ import { should, use } from "chai";
 import * as chaiAsPromised from "chai-as-promised";
 import * as chaiThings from "chai-things";
 import * as pino from "pino";
+import { SinonSandbox } from "sinon";
 import * as vscode from "vscode";
 
 use(chaiThings);
@@ -12,6 +13,36 @@ export const logger = pino(
   { level: "trace" },
   pino.destination("./logfile.json")
 );
+
+export interface FakeEvent<T> {
+  listeners: Array<(e: T) => void>;
+
+  fire: (e: T) => void;
+
+  event: (listener: (e: T) => void) => vscode.Disposable;
+}
+
+export function makeFakeEvent<T>(): FakeEvent<T> {
+  const listeners: Array<(e: T) => void> = [];
+
+  const fire = (e: T) => {
+    listeners.forEach(listener => listener(e));
+  };
+
+  const event = (listener: (e: T) => void) => {
+    listeners.push(listener);
+    return { dispose: () => {} };
+  };
+
+  return { listeners, event, fire };
+}
+
+export const createStubbedVscodeWindow = (sandbox: SinonSandbox) => ({
+  showErrorMessage: sandbox.stub(),
+  showInformationMessage: sandbox.stub(),
+  showInputBox: sandbox.stub(),
+  showQuickPick: sandbox.stub()
+});
 
 export async function waitForEvent<T>(
   event: vscode.Event<T>
