53303ede21d02c04fe96716e37e225b9227acaa8
Dan Čermák <dcermak@suse.com>
2020-09-14 19:00:16 +0200

    Add temporary workaround for a non existent gem in start-mini-obs

--- start-mini-obs.sh
+++ start-mini-obs.sh
@@ -20,6 +20,8 @@ git submodule update
 
 # don't build the containers for testing, we don't need them and it just takes ages
 sed -i '/docker-compose.*docker-compose\.yml.*docker-compose\.minitest\.yml.*docker-compose\.minitest-user\.yml.*build.*minitest/d' Rakefile
+# temporary workaround for codecov 0.1.14 no longer being available
+sed -i 's/codecov (0\.1\.14)/codecov (0.2.8)/' src/api/Gemfile.lock
 rake docker:build
 docker-compose up -d
 
