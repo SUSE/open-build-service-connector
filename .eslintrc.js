3ff40a9c79cc96ca12c92d57c2e28f91edda4bcc
Dan Čermák <dcermak@suse.com>
2020-10-08 17:15:34 +0200

    sync eslint settings with backend

--- .eslintrc.js
+++ .eslintrc.js
@@ -7,16 +7,19 @@ module.exports = {
   },
   plugins: ["@typescript-eslint"],
   rules: {
-    "no-unused-vars": [
-      "error",
-      { argsIgnorePattern: "^_", varsIgnorePattern: "^_" }
-    ],
-    "@typescript-eslint/unbound-method": "off",
     "@typescript-eslint/no-inferrable-types": "off",
     "@typescript-eslint/no-floating-promises": "error",
     "@typescript-eslint/array-type": "error",
     "@typescript-eslint/consistent-type-definitions": ["error", "interface"],
-    "@typescript-eslint/no-extra-non-null-assertion": ["error"]
+    "@typescript-eslint/restrict-template-expressions": "warn",
+    // "@typescript-eslint/no-unused-vars"
+    "no-unused-vars": ["error", { varsIgnorePattern: "^_.*" }],
+    "@typescript-eslint/no-extra-non-null-assertion": ["error"],
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unsafe-member-access": "off",
+    // we use so many callbacks that get an explicit `this`, making this
+    // completely unusable
+    "@typescript-eslint/unbound-method": "off"
   },
   extends: [
     "eslint:recommended",
