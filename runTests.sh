daa217ad1ee1c0dbf190997886d006009b80b63b
Dan Čermák <dcermak@suse.com>
2020-10-09 11:06:53 +0200

    [ci] Run the UI tests on Travis
    - remove github action for the ui tests & remove the badge
    - don't run the travis tests on node 10
    - add --quiet flag to runTests.sh so that it does not overflow travis' stdout

--- runTests.sh
+++ runTests.sh
@@ -5,6 +5,8 @@ set -ox pipefail
 rm logfile.json
 node ./out/test/runTest.js
 retval=$?
-pino-pretty < logfile.json
+if [[ "$1" != "--quiet" ]]; then
+  pino-pretty < logfile.json
+fi
 
 exit $retval
