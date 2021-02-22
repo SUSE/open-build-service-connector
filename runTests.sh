a912cb14103c9da5a76c5ec0c1382a29bd7355ee
Dan Čermák <dcermak@suse.com>
2021-02-22 16:02:25 +0100

    Use webpack development mode for the tests
    This speeds up development

--- runTests.sh
+++ runTests.sh
@@ -2,7 +2,13 @@
 
 set -ox pipefail
 
+yarn run mocklibsecret
+export EXTENSION_DEBUG=1
+
 rm logfile.json
+mkdir -p test-home
+export HOME="$(pwd)/test-home"
+export LD_PRELOAD="$(pwd)/mocklibsecret/build/libsecret.so"
 node ./out/test/runTest.js
 retval=$?
 pino-pretty < logfile.json
--- runUiTests.sh
+++ runUiTests.sh
@@ -2,10 +2,13 @@
 
 set -euox pipefail
 
+export EXTENSION_DEBUG=1
+
 yarn run compile
 yarn run mocklibsecret
 
-for dir in $(ls -d src/ui-tests/*/);do
+# for dir in $(ls -d src/ui-tests/*/); do
+for dir in "src/ui-tests/accounts/"; do
   testname=$(basename "${dir}")
   node ./out/ui-tests/runTests.js "${testname}"
 done
