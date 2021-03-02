2fe2e52e49ded5dbe71135579fd1245fd242a02d
Dan Čermák <dcermak@suse.com>
2021-03-02 17:59:51 +0100

    [tests] Stop printing out the test log file after a run

--- runTests.sh
+++ runTests.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-set -ox pipefail
+set -eox pipefail
 
 yarn run mocklibsecret
 export EXTENSION_DEBUG=1
 
-rm logfile.json
+rm -f logfile.json
 mkdir -p test-home
 export HOME="$(pwd)/test-home"
 export LD_PRELOAD="$(pwd)/mocklibsecret/build/libsecret.so"
 node ./out/test/runTest.js
-retval=$?
-pino-pretty < logfile.json
-
-exit $retval
