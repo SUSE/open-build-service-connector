8b8c539528381637eaf6bad1b2a766c581fa891a
Dan Čermák <dcermak@suse.com>
2021-04-09 20:16:17 +0200

    Add emacs dir-locals w/ projectile settings
    Yep, I use emacs to write this :-P

---
+++ .dir-locals.el
@@ -0,0 +1,9 @@
+;;; Directory Local Variables
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil
+  (projectile-project-test-cmd . "yarn run coverage")
+  (projectile-project-configure-cmd . "yarn run clean && yarn run cleandeps && yarn install")
+  (projectile-project-compilation-cmd . "yarn run compile")
+  )
+ )
